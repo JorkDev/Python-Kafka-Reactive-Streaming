09363fab51cf852ca976c39d28d9343bf7528c81
JorkDev <jorkaefdev@gmail.com>
2024-03-02 23:04:17 -0500

    Update config template with kafka parameters

--- example_config.py
+++ example_config.py
@@ -1,4 +1,11 @@
 config = {
-    "google_api_key": "",
-    "youtube_playlist_id": ""
+    "google_api_key": "AIzaSyAhQlRcfLxp2ma9ynKKRrmeOe0McJF_XkA",
+    "youtube_playlist_id": "PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU",
+    "kafka": {
+        "bootstrap.servers": "...",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "...",
+        "sasl.password": "...",
+    }
 }
