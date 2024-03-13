689dff6569561f153008be401981e68901b86c29
JorkDev <jorkaefdev@gmail.com>
2024-03-13 11:45:45 -0500

    Added support for Avro serialization in YouTube data production

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -6,6 +6,10 @@ import requests
 from config import config
 import json
 from pprint import pformat
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka import SerializingProducer
 
 def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=None):
     response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems",
@@ -53,17 +57,27 @@ def summarize_video(video):
     return {
         "video_id": video["id"],
         "title": video["snippet"]["title"],
-        "views": video["statistics"].get("viewCount",0),
-        "likes": video["statistics"].get("likeCount",0),
-        "comments": video["statistics"].get("commentCount",0),
+        "views": int(video["statistics"].get("viewCount", 0)),
+        "likes": int(video["statistics"].get("likeCount", 0)),
+        "comments": int(video["statistics"].get("commentCount", 0)),
     }
 
-def ondelivery(err, record):
+def on_delivery(err, record):
     pass
 
 def main():
     logging.info("START")
-    kafka_config = config["kafka"]
+
+    schema_registry_client = SchemaRegistryClient(config["schema_registry"])
+    youtube_videos_value_schema = schema_registry_client.get_latest_version("youtube_videos-value")
+
+    kafka_config = config["kafka"] | {
+        "key.serializer": StringSerializer(),
+        "value.serializer": AvroSerializer(
+            schema_registry_client,
+            youtube_videos_value_schema.schema.schema_str,
+        ),
+    }
     producer = SerializingProducer(kafka_config)
 
     google_api_key = config["google_api_key"]
@@ -74,12 +88,19 @@ def main():
         for video in fetch_videos(google_api_key, video_id):
             logging.info("GOT %s", pformat(summarize_video(video)))
 
-        producer.produce(
-            topic="youtube_videos",
-            key= video_id,
-            value = value,
-            on_delivery = ondelivery
-        )
+            producer.produce(
+                topic="youtube_videos",
+                key=video_id,
+                value={
+                    "TITLE": video["snippet"]["title"],
+                    "VIEWS": int(video["statistics"].get("viewCount", 0)),
+                    "LIKES": int(video["statistics"].get("likeCount", 0)),
+                    "COMMENTS": int(video["statistics"].get("commentCount", 0)),
+                },
+                on_delivery=on_delivery,
+            )
+
+    producer.flush()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
